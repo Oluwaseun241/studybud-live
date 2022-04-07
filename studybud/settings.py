4572398f78fa97c3c3c83130cbc6b486f456f56c
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2022-04-07 03:20:58 +0100

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cs&htqyi_^+m^hk&z1cvrux2jf$z)+l-prfp$3*!j0u+$7z&3*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studybud-live.herokuapp.com','127.0.0.1']
 
 
 # Application definition
