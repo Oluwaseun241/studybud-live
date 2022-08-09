ce3190f0d7515fb853b66c9ff02308f6593890c1
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2022-08-09 17:13:37 +0100

    updated DB

--- studybud/settings.py
+++ studybud/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '5PCY2FUUdKMBimBeBr2x',
+        'HOST': 'containers-us-west-86.railway.app',
+        'PORT': '6874',
     }
 }
 
