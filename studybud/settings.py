b9e33070872539bb9545a3006bdb12a4bc21ad17
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2022-08-09 17:58:36 +0100

    edit

--- studybud/settings.py
+++ studybud/settings.py
@@ -142,20 +142,20 @@ USE_TZ = True
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
-# MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = "/images/"
-django_heroku.settings(locals())
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = "/static/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_URL = "/images/"
+# django_heroku.settings(locals())
 
 # STATIC_ROOT =
 
